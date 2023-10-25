77449c5879ec6f5d6ab5198a06ea7fbbfede52aa
ellenmonster <ellenmonster@gmail.com>
2023-10-24 22:29:07 -0600

    updated files

--- DM_training_script.R
+++ DM_training_script.R
@@ -1,21 +1,31 @@
-## Save your script...
+### IMD DATA MANAGERS TRAINING
+### INTRO. TO R     
+### OCTOBER 25, 2023
 
-# Load tidyverse
+## Getting started ----
+# Clear the global environment
+rm(list = ls()) 
+
+# Load packages
 library(tidyverse)
+
+## Working with vectors ----
+
+
+## Working with data frames ----
 
 # Import the park_visits data
 dat <- read_csv("Data_in/park_visits.csv")
 
 # Check the data
-glimpse(dat)
 summary(dat)
 
-# Extract a subset of the data (using dplyr functions and piping instead of base R)
+# Show them the plot we want to create (the end goal) and outline the steps to get there...
 
-# What are our choices for region names?
+# What are our choices for region names, and how many records per region?
 table(dat$region)
 
-# Just get data for the MW region
+# Just get data for the MW region (dplyr and piping)
 subdat <- dat %>%
   dplyr::filter(region == "MW" & year != "Total") %>%
   dplyr::select(year, unit_code, unit_name, visitors) %>%
@@ -24,14 +34,17 @@ subdat <- dat %>%
     visitors_1000 = visitors/1000) %>%
   dplyr::arrange(unit_code, year)
 
-# plot the data
-visitors_plot <- ggplot(subdat, aes(year, visitors_1000, colour = unit_code)) + 
+## Plot the data ----
+
+# Simple plot, with base R
+
+visitors_plot <- ggplot(subdat, aes(x = year, y = visitors_1000, colour = unit_code)) + 
   geom_line() +
   labs(title = "Annual number of visitors to National Parks in Midwest Region", x = "Year", y = "Visitors (1000's)") +
   theme_bw()
 
-# save the plot (easy way)
-# **Plots > Export > Save as Image (choose location, filename, image type)
+# Save the plot (easy way)-- create a subfolder called Plots, and size the plots pane for preferred export size
+# **Plots > Export > Save as Image (choose location, filename, image type) 
 
 # And JUST FOR FUN from the plotly package (need to install and load this package)
 plotly::ggplotly(visitors_plot)
